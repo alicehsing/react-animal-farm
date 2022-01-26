6b0542bdf9e69c43d59be9fb14bc9c0b283e1bca
Alice Hsing <alicehsing@gmail.com>
2022-01-25 16:08:36 -0800

    got rid of white in the background image

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,6 +2,5 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer(props) {
-  return <footer>For questions, please reach out to {props.email} 
-  </footer>;
+  return <footer>For questions, please reach out to {props.email}.</footer>;
 }
