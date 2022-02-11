c17fe5d524e82e9c2308da04269f4bd6ba744994
Alice Hsing <alicehsing@gmail.com>
2022-02-10 16:35:10 -0800

    refactor App.js, Main.js, Header.js to class components

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
 import './Header.css';
 
-export default function Header(props) {
-  return <header>Hi, {props.greeting}! Welcome to Alchemy Animal Farm!</header>;
+export default class Header extends Component {
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+
+  //   };
+  // }
+
+  render() {
+    const { greeting } = this.props;
+
+    return <header>Hi, {greeting}! Welcome to Alchemy Animal Farm!</header>;
+  }
 }
+
+// export default function Header(props) {
+//   return <header>Hi, {props.greeting}! Welcome to Alchemy Animal Farm!</header>;
+// }
