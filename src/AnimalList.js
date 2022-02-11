9f8908973bd4037087a54367d41cdfc71e115078
Alice Hsing <alicehsing@gmail.com>
2022-02-10 17:11:07 -0800

    refactored function based componets to class based componets

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
 import Animal from './Animal/Animal.js';
 
-export default function AnimalList(props) {
-  return <div>
-    {props.animals.map((animal, i) =>
+export default class AnimalList extends Component {
+
+  render() {
+    const { animals } = this.props;
+    return <div> {animals.map((animal, i) =>
       <Animal key={animal.name + i} name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} color={animal.color} />
     )}
-  </div>;
+    </div>;
+  }
 }
+
+// export default function AnimalList(props) {
+//   return <div>
+//     {props.animals.map((animal, i) =>
+//       <Animal key={animal.name + i} name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} color={animal.color} />
+//     )}
+//   </div>;
+// }
